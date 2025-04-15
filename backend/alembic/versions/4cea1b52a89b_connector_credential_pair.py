26ec9520559e3ca7dd3630a2ff84e8240240a7cc
Alex Usher <alex@dialin-ai.com>
2025-04-14 20:55:16 -0700

    fixing base deployment

---
+++ backend/alembic/versions/4cea1b52a89b_connector_credential_pair.py
@@ -0,0 +1,23 @@
+"""Polling Document Count
+
+Revision ID: 4cea1b52a89b
+Revises: d961aca62eb3
+Create Date: 2025-04-15 12:00:00.000000
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = "4cea1b52a89b"
+down_revision = "d961aca62eb3"
+branch_labels: None = None
+depends_on: None = None
+
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
